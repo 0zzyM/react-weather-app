1101e3e7dce46b401b5d61d7bd49cee06dadc4e5
0zzyM <ozzymacar@gmail.com>
2026-06-23 12:12:37 +0200

    chore(navbar): migrate navbar to typescript

---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,38 @@
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+
+interface NavbarProps {
+  isNightMode: boolean;
+  setIsNightMode: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Navbar = ({ isNightMode, setIsNightMode }: NavbarProps) => {
+  return (
+    <nav className={`header-wrap ${isNightMode ? "night" : ""}`}>
+      <ul className="logo-wrapper">
+        <li className="logo-item">
+          <Link to="/">weatherApp</Link>
+        </li>
+      </ul>
+      <ul className="navbar">
+        <li className="navbar-item">
+          <Link to="/">Current Weather</Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="forecast">5 Day Forecast</Link>
+        </li>
+        <li className="night-mode-toggle-wrapper">
+          <p>Night Mode</p>
+          <button
+            className={`night-mode-toggle-btn ${isNightMode ? "toggled " : ""}`}
+            onClick={() => setIsNightMode(!isNightMode)}
+          >
+            <div className="thumb"></div>
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
